c3d115eea1ab1d373e5aee171691f4199aeed72e
Isachi25 <sharonachiengmasiga@gmail.com>
2024-03-27 16:26:44 -0700

    Checks if a binary tree is complete

---
+++ 102-binary_tree_is_complete.c
@@ -0,0 +1,52 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - Measures the size of a binary tree
+ * @tree: The binary tree input
+ * Return: The no. of descendant child nodes
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+
+	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
+}
+
+/**
+ * is_complete - A helper function for binary_tree_is_complete
+ * @tree: a pointer to the root of the tree
+ * @index: The index of current node
+ * @size: The total number of nodes in the tree
+ * Return: 1 if true 0 if false
+ */
+_Bool is_complete(const binary_tree_t *tree, unsigned int index, size_t size)
+{
+	if (!tree)
+		return (true);
+
+	if (index >= size)
+		return (false);
+
+	return (is_complete(tree->left, 2 * index + 1, size) &&
+		is_complete(tree->right, 2 * index + 2, size));
+}
+
+
+/**
+ * binary_tree_is_complete - A function that checks if a binary tree is
+ * complete
+ * @tree: A pointer to root of tree
+ * Return: 1 if true 0 if false
+ */
+int binary_tree_is_complete(const binary_tree_t *tree)
+{
+	size_t size;
+	unsigned int i = 0;
+
+	if (!tree)
+		return (0);
+
+	size = binary_tree_size(tree);
+	return (is_complete(tree, i, size));
+}
